649ddb6c3448a7f4d8cbb0ad5eedc28b4fbaf9ba
Axonn1018 <ktruong1018@gmail.com>
2024-05-24 08:54:30 -0400

    Reformatting

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { User, UserService } from '../user.service';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { User, UserService } from "../user.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
-
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
+    email: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required]),
   });
 
   // login(){
@@ -20,10 +19,12 @@ export class LoginComponent {
   //   console.log("The password is :", this.loginForm.controls.password.value);
   // }
 
-
-  constructor(private userService : UserService) {}
+  constructor(private userService: UserService) {}
 
   login() {
-    this.userService.authenticate(this.loginForm.controls.email.value || '', this.loginForm.controls.password.value || '') ;
+    this.userService.authenticate(
+      this.loginForm.controls.email.value || "",
+      this.loginForm.controls.password.value || ""
+    );
   }
 }
